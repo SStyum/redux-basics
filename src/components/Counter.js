b63bdb2cdf6ade10d43a1cc9c204c4b74e4e5cb8
Guilherme Rocha <guirocha1938@gmail.com>
2024-10-05 18:41:55 -0300

    feat: Creates store, sets initial state values and initial actions to counter.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,36 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: 'INCREMENT' });
+  }
+
+  const increaseHandler = () => {
+    dispatch({ type: 'INCREASE', amount: 5 });
+  }
+
+  const decrementHandler = () => {
+    dispatch({ type: 'DECREMENT' });
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'TOGGLE' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>INCREMENT</button>
+        <button onClick={increaseHandler}>INCREASE BY 5</button> 
+        <button onClick={decrementHandler}>DECREMENT</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
